5023bf6a7e62aa1d30e6b9607240fd6e3dcef799
chapmanb <chapmanb@50mail.com>
2018-08-22 11:43:38 +0000

    Move dx-cwl and yq into bcbio-vm asset, clean up workflow
    Finalize #5
    
    - Update shell script to remove old code and workarounds now
      part of bcbio-vm asset.
    - Provides dx-cwl and yq though conda packages as part of bcbio-vm
      asset.

--- bcbio-run-workflow/src/bcbio-run-workflow.sh
+++ bcbio-run-workflow/src/bcbio-run-workflow.sh
@@ -3,15 +3,12 @@ set -e -x -o pipefail
 #!/bin/bash
 main() {
 
-    #BCBIO_CONTAINER="record-FJ2P25j0f5vJxx3K95BVVx2f"
     # This container file is the actual bundled file record-FJ2P25j0f5vJxx3K95BVVx2f is poiting to.
     # bcbio-run-workflow app requires the fileID for dx-cwl to compile the workflow 
-    BCBIO_CONTAINER_FILE="file-FJ2P1Gj0f5vBkqJ67G6VxBZf" 
+    BCBIO_CONTAINER_FILE="file-FJkVkkj0f5v8QzVg835Z8j7P" 
 
     if [ "$pull_from_docker_registry" = "true" ]; then
         BCBIO_ASSETS=""
-    elif [ "${BCBIO_CONTAINER}" != "" ];  then
-        BCBIO_ASSETS="--assets $BCBIO_CONTAINER"
     elif [ "${BCBIO_CONTAINER_FILE}" != "" ]; then
         BCBIO_ASSETS="--bundled $BCBIO_CONTAINER_FILE"
     fi
@@ -20,23 +17,9 @@ main() {
     dx download "$sample_spec" -o sample_spec.csv
     dx download "$system_configuration" -o system_configuration.yml
 
-    # version of requests shipping with DNAnexus incompatible with newer conda
-    #PYTHONPATH_BACKUP=$PYTHONPATH
-    #unset PYTHONPATH
-    #bcbiovm_conda install -y -c conda-forge yq
-    #export PYTHONPATH=$PYTHONPATH_BACKUP
-    #ln -s /install/bcbio-vm/anaconda/bin/yq /usr/local/bin/yq
     PNAME=`yq -r .dnanexus.project < system_configuration.yml`
 
     mv sample_spec.csv ${PNAME}.csv
-
-    #wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
-    #bash Miniconda2-latest-Linux-x86_64.sh -b -p ~/install/bcbio-vm/anaconda
-    #~/install/bcbio-vm/anaconda/bin/conda install --yes -c conda-forge -c bioconda bcbio-nextgen-vm
-    #ln -s ~/install/bcbio-vm/anaconda/bin/bcbio_vm.py /usr/local/bin/bcbio_vm.py
-    #ln -s ~/install/bcbio-vm/anaconda/bin/conda /usr/local/bin/bcbiovm_conda
-    #ln -s ~/install/bcbio-vm/anaconda/bin/python /usr/local/bin/bcbiovm_python
-
 
     source /home/dnanexus/environment
     set +x
@@ -50,22 +33,16 @@ main() {
     export PATH=/usr/local/bin:$PATH
     ls /usr/local/bin
 
-    # Ignore PYTHONPATH set by DNAnexus instance, which introduces incompatible libraries
-    sed -i 's|bcbio-vm/anaconda/bin/python|bcbio-vm/anaconda/bin/python -Es|' /usr/local/bin/bcbio_vm.py
     bcbio_vm.py template --systemconfig system_configuration.yml yaml_template.yml $PNAME.csv
     bcbio_vm.py cwl --systemconfig system_configuration.yml $PNAME/config/$PNAME.yaml
     tar -cvzf $PNAME-generated-cwl.tgz $PNAME-workflow/main-$PNAME.cwl
     
-    #git clone https://github.com/dnanexus/dx-cwl.git
-    mkdir -p /home/dnanexus/dx-cwl
-    mv /packages/dx-cwl /home/dnanexus/
-
     if [ -n "$reuse_workflow" ]; then
         WF_ID_OR_NAME=$reuse_workflow
     else
         WF_ID_OR_NAME="/$output_folder/main-$PNAME/main-$PNAME"
         set +x
-        bcbiovm_python dx-cwl/dx-cwl compile-workflow $PNAME-workflow/main-$PNAME.cwl --project $DX_PROJECT_ID --token $DX_AUTH_TOKEN ${BCBIO_ASSETS} --rootdir $output_folder
+        dx-cwl compile-workflow $PNAME-workflow/main-$PNAME.cwl --project $DX_PROJECT_ID --token $DX_AUTH_TOKEN ${BCBIO_ASSETS} --rootdir $output_folder
         set -x
         generated_cwl=$(dx upload $PNAME-generated-cwl.tgz --brief)
         dx-jobutil-add-output generated_cwl "$generated_cwl" --class=file
@@ -76,6 +53,6 @@ main() {
         sleep 5
     fi
     set +x
-    bcbiovm_python dx-cwl/dx-cwl run-workflow $WF_ID_OR_NAME /$output_folder/main-$PNAME-samples.json --project $DX_PROJECT_ID --token $DX_AUTH_TOKEN --rootdir $output_folder
+    dx-cwl run-workflow $WF_ID_OR_NAME /$output_folder/main-$PNAME-samples.json --project $DX_PROJECT_ID --token $DX_AUTH_TOKEN --rootdir $output_folder
     set -x
 }
